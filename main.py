4a831efc1e152279cb945c7e927f78416989bb94
Caio Garcia <caio.augusto@algar.com.br>
2023-04-11 16:51:30 -0300

    Implements consuming test CSV file and returning as dict at endpoint.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from typing import Union
 
 from fastapi import FastAPI
 
+import pandas
+
 app = FastAPI()
 
 
@@ -13,7 +15,7 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: str, q: Union[str, None] = None):
     """
     Test endpoint with argument.
 
@@ -21,3 +23,16 @@ def read_item(item_id: int, q: Union[str, None] = None):
     Used for testing purposes.
     """
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/csv")
+def read_csv():
+    """
+    Get csv from directory.
+
+    First implementation of consuming a CSV file.
+    This will read a specific CSV file from the project directory,
+    which is in the expected format to be received by the API.
+    """
+    df = pandas.read_csv("test-input.csv").T.to_dict()
+    return df
